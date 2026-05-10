34ac07969b5a0a688516d3b399f587284e235966
Rafael JPD <rafael@pitangainnovare.com.br>
2026-05-10 09:43:29 -0300

    remove param until date de logger preprint

--- proc/collect_preprint_dictionary.py
+++ proc/collect_preprint_dictionary.py
@@ -83,7 +83,7 @@ def main():
         'from': params.from_date
     })
 
-    logging.info('Obtendo dados do OAI-PMH Preprints para (%s,%s)' % (params.from_date, params.until_date))
+    logging.info('Obtendo dados do OAI-PMH Preprints para (%s,%s)' % (params.from_date))
     data = {}
     for r in records:
         data.update(parse(r))
